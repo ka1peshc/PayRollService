e5806c88a36c2dc85d4a215a732a85dd0e2d011c
ka1peshc <kalpesh111997@gmail.com>
2021-10-14 07:27:49 +0530

    Add: Welcome message

---
+++ PayrollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Payroll service");
+        }
+    }
+}
